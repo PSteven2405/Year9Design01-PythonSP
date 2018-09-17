fa00cb1c4d40f9bb4fc37511d7f8a659eaf4f986
David Theondra Steven Prasetya <steven.prasetya@10246.local>
2018-09-17 13:54:02 -0400

    MathExample Update

---
+++ MathExample01.py
@@ -0,0 +1,10 @@
+#These are comments in Java. When you program
+#and I look at your work I always check your comment
+x = 1 + 2 #This creates a variable called x and puts 1 + 2 in it
+print(x)
+
+#Assignment statement
+#An Assignment statement is any situation where I have
+#s <something> = <statement>
+x = x + 1
+print(x)
